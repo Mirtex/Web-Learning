0e1fcda6361fb4f88bb7b01f9cbcb88108924a82
Ernesto Eusebio <erareu@gmail.com>
2019-07-24 23:15:24 -0400

    PHP Introduction
    - Adds PHP introduction directory
    - Adds Initial intro.php testing file

---
+++ PHP/intro.php
@@ -0,0 +1,10 @@
+<?php
+$firstName = "Ernesto";
+$lastName = "Eusebio";
+$age = 31;
+$cash = 105.50;
+
+echo $firstName, ' ', $lastName, " Age: "; //Echo can print multiple values
+print $age; //Print can only print one value
+
+?>
